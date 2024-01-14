bf7fe3a154df1c4db900cc3b75051eb82f77e998
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-01-14 20:46:14 +0500

    I/E components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ import HeroAbout from "./hero-about";
 import Vector from "./vector";
 import Hero from "./hero";
 import Service from "./service";
+import Pricing from "./pricing";
+import Subscribe from "./subscribe";
 
-export { HeroAbout, Vector, Hero, Service };
+export { HeroAbout, Vector, Hero, Service, Pricing, Subscribe };
